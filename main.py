604dae2e37ec2b09db6ee90f9054f48bcc0cb66e
KirillDrago <dragoswim7@gmail.com>
2023-03-03 15:40:17 +0200

    Added script for archive and pinned groups in Telegram

---
+++ main.py
@@ -0,0 +1,42 @@
+import asyncio
+import os
+import time
+
+from telethon import TelegramClient
+from dotenv import load_dotenv
+from telethon.tl.functions.messages import ToggleDialogPinRequest
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+PHONE = os.getenv("PHONE")
+
+
+client = TelegramClient("session", API_ID, API_HASH)
+
+
+async def main():
+    await client.connect()
+    if not await client.is_user_authorized():
+        await client.send_code_request(PHONE)
+        await client.sign_in(PHONE, input("Введите код из СМС: "))
+
+    chats = await client.get_dialogs()
+    groups = []
+    for chat in chats:
+        if chat.is_group:
+            groups.append(chat)
+
+    for group in groups:
+        await client.edit_folder(group, 1)
+        time.sleep(1)
+        await client(ToggleDialogPinRequest(
+            peer=group,
+            pinned=True
+        ))
+        print(f"{group.title} добавлен в архив и закреплен")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
